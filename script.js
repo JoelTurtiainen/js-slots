e8ad90ba8b7aa49a0cf47ade9f798de2d5dd631c
Joel Turtiainen <joel.turtiainen@gmail.com>
2024-06-28 07:57:04 +0300

    Release

--- script.js
+++ script.js
@@ -1,22 +1,41 @@
-// ★ Credits for youtube@CodeJos for the main functionality ★
-const red = '\x1b[31m',
-	green = '\x1b[32m',
-	yellow = '\x1b[33m';
+// ★ Credits for youtube@CodeJos for the roll And rollAll ★
+
+// ## Setup for reels
+let indexes = [0, 0, 0, 0];
+const numIcons = 6;
+const timePerIcon = 100;
+const iconHeight = 100;
+const fruits = [
+	{ name: 'pear', mult: 4 },
+	{ name: 'banana', mult: 7 },
+	{ name: 'seven', mult: 10 },
+	{ name: 'melon', mult: 5 },
+	{ name: 'apple', mult: 6 },
+	{ name: 'cherry', mult: 3 },
+];
+
+// ## Dom Cache
+
+const [displayBet, displayWin, displayCredits] = document.querySelectorAll('.display-bet, .display-win, .display-credits');
+const [panel, rollBtn] = document.querySelectorAll('.btn-panel, .roll');
+const [betUp, betDown] = document.querySelector('.bet-actions').children;
+const lockBtns = document.querySelectorAll('.toggle-btn');
+const reelList = document.querySelectorAll('.reel-container > .reel');
+
+// ## Constructor
 
 class Progress {
-	#credits = 10;
+	#credits = 20;
 	#bet = 1;
+	#win = 0;
 
 	set bet(val) {
 		// HACK: Make lock buttons unavailable if bet changes
-		toggleBtns(true);
-
+		enableLockBtns(false);
 		if (val > 0) {
 			this.#bet = val;
-
-			console.debug(`NEW BET: ${val} mk`);
-			document.querySelector('.bet').textContent = this.#bet;
 		}
+		this.updateDom();
 	}
 
 	get bet() {
@@ -24,42 +43,35 @@ class Progress {
 	}
 
 	set credits(val) {
-		const old = this.#credits;
+		const result = val - this.#credits;
+		this.#win = result > 0 ? result : 0;
 		this.#credits = val;
-		document.querySelector('.credits').textContent = this.#credits;
-
-		if (this.#credits > old) console.debug(`${green}ADDED: ${this.credits - old} mk`);
-		else console.debug(`${red}REMOVED: ${this.credits - old} mk`);
+		this.updateDom();
 	}
 
 	get credits() {
-		document.querySelector('.credits').textContent = this.#credits;
 		return this.#credits;
+	}
+
+	updateDom() {
+		displayBet.firstElementChild.textContent = this.#bet;
+		displayWin.firstElementChild.textContent = this.#win;
+		displayCredits.firstElementChild.textContent = this.#credits;
+
+		betDown.disabled = this.#bet <= 1;
+		betUp.disabled = this.#bet >= this.#credits;
+		rollBtn.disabled = this.#bet > this.#credits;
 	}
 }
 
-const iconWidth = 96,
-	iconHeight = 96,
-	numIcons = 6,
-	timePerIcon = 100,
-	fruits = [
-		{ name: 'pear', mult: 4 },
-		{ name: 'banana', mult: 7 },
-		{ name: 'seven', mult: 10 },
-		{ name: 'melon', mult: 5 },
-		{ name: 'apple', mult: 6 },
-		{ name: 'cherry', mult: 3 },
-	];
-
-// TODO: Make this constant before release
-let indexes = [0, 0, 0, 0];
+// ## Slot Reel Functionality
 
 const roll = (reel, offset = 0) => {
 	const delta = (offset + 2) * numIcons + Math.round(Math.random() * numIcons);
-	const style = getComputedStyle(reel),
-		backgroundPositionY = parseFloat(style['background-position-y']),
-		targetBackgroundPositionY = backgroundPositionY + delta * iconHeight,
-		normTargetBackgroundPositionY = targetBackgroundPositionY % (numIcons * iconHeight);
+	const style = getComputedStyle(reel);
+	const backgroundPositionY = parseFloat(style['background-position-y']);
+	const targetBackgroundPositionY = backgroundPositionY + delta * iconHeight;
+	const normTargetBackgroundPositionY = targetBackgroundPositionY % (numIcons * iconHeight);
 
 	return new Promise((resolve, reject) => {
 		reel.style.transition = `background-position-y ${8 + delta * timePerIcon}ms`;
@@ -76,92 +88,110 @@ const roll = (reel, offset = 0) => {
 };
 
 function rollAll() {
-	const checkboxes = document.querySelectorAll('.button-container > input[type=checkbox]');
-	const rerolled = [...checkboxes].some((element) => element.checked);
-	const reelList = document.querySelectorAll('.reel-container > .reel');
-	const promises = Array.from(reelList, (reel, i) =>
-		checkboxes[i].checked ? Promise.resolve(0) : roll(reel, i),
-	);
+	const promises = Array.from(reelList, (reel, i) => (lockBtns[i].ariaPressed === 'true' ? Promise.resolve(0) : roll(reel, i)));
 
-	// Disable for duration of the roll
-	document.querySelector('.roll').setAttribute('disabled', true);
+	// Disable for duration of promise
+	enableActionButtons(false);
 
 	Promise.all(promises).then((deltas) => {
 		deltas.forEach((delta, i) => (indexes[i] = (indexes[i] + delta) % numIcons));
 
-		// Enable Roll button again
-		document.querySelector('.roll').removeAttribute('disabled');
+		reelList.forEach((reel, i) => (reel.ariaLabel = fruits[indexes[i]].name));
 
 		const result = checkWin();
 
 		if (result > 0) {
-			document.querySelector('.button-container').classList.add('blink');
-			setTimeout(() => document.querySelector('.button-container').classList.remove('blink'), 2000);
+			panel.classList.add('blink');
+			setTimeout(() => panel.classList.remove('blink'), 2000);
 			progress.credits += result;
+			enableLockBtns(false);
+		} else {
+			enableLockBtns(true);
 		}
-		toggleBtns(rerolled, result);
+
+		enableActionButtons(true);
+		progress.updateDom();
 	});
 }
 
-function toggleBtns(rerolled = false, result = 0) {
-	const btns = document.querySelectorAll('.button-container > input[type=checkbox]');
-	// Disable / Enable 'Lock' buttons
-	if (rerolled || result > 0) {
-		btns.forEach((btn) => btn.setAttribute('disabled', true));
-	} else {
-		btns.forEach((btn) => btn.removeAttribute('disabled'));
-	}
-	// Uncheck 'Lock' Buttons
-	document.querySelectorAll('input:checked').forEach((input) => (input.checked = false));
-}
-
 function checkWin() {
-	let result = 0;
 	const counts = {};
-	// Turn into dict with counts
+	// Turn into object with counts
 	indexes.forEach((index) => {
 		counts[index] = counts[index] ? counts[index] + 1 : 1;
 	});
 
-	if (Object.values(counts).includes(4)) {
-		result = Number(fruits[Object.keys(counts)[0]].mult * progress.bet);
-	} else if (counts['2'] === 3) {
-		result = Number(fruits[2].mult * (progress.bet / 2));
+	const hasThree = Object.values(counts).some((count) => count === 3);
+	const hasFour = Object.values(counts).some((count) => count === 4);
+
+	if (hasFour) {
+		const winningIndex = Object.keys(counts).find((index) => counts[index] === 4);
+		return fruits[winningIndex].mult * progress.bet;
+	} else if (hasThree) {
+		// index '2' is for Seven
+		if (counts['2'] === 3) {
+			return fruits[2].mult * (progress.bet / 2);
+		}
 	}
-	return result;
+	return 0;
 }
+
+// ## Button Logic
+
+function enableActionButtons(bool) {
+	const btns = [rollBtn, betUp, betDown];
+	if (bool) {
+		btns.forEach((btn) => (btn.disabled = false));
+		panel.removeAttribute('style');
+	} else {
+		btns.forEach((btn) => (btn.disabled = true));
+		panel.style.pointerEvents = 'none';
+	}
+}
+
+function enableLockBtns(bool) {
+	const rerolled = [...lockBtns].some((element) => element.ariaPressed === 'true');
+
+	// Uncheck 'Lock' Buttons
+	lockBtns.forEach((element) => toggleButtons(element, true));
+
+	// Disable / Enable 'Lock' buttons
+	if (bool === false || rerolled) {
+		lockBtns.forEach((element) => (element.disabled = true));
+	} else {
+		lockBtns.forEach((element) => (element.disabled = false));
+	}
+}
+
+// ## Events
 
 function onClick(e) {
 	const classes = e.target.classList;
 	// console.log(classes);
-
-	if (classes.contains('roll') && progress.bet <= progress.credits) {
+	if (classes.contains('toggle-btn')) {
+		toggleButtons(e.target);
+	} else if (classes.contains('roll') && progress.bet <= progress.credits) {
 		progress.credits -= progress.bet;
 		rollAll();
-	} else if (e.target.id === 'betUp') {
+	} else if (classes.contains('bet-up')) {
 		progress.bet += 1;
-	} else if (e.target.id === 'betDown') {
+	} else if (classes.contains('bet-down')) {
 		progress.bet -= 1;
 	}
 }
 
-function init() {
-	const slots = document.querySelector('body');
-	slots.addEventListener('click', onClick);
+function toggleButtons(element, override = false) {
+	const isPressed = element.getAttribute('aria-pressed') === 'true' || override;
+	element.setAttribute('aria-pressed', String(!isPressed));
+	element.textContent = isPressed ? 'Lock' : 'Locked'; // Toggle button text
+}
 
+// ## Init
+
+function init() {
+	document.querySelector('.btn-panel').addEventListener('click', onClick);
 	return new Progress();
 }
 
 const progress = init();
-
-addEventListener('keydown', (e) => {
-	switch (e.key) {
-		case 'q':
-			fruits.forEach((icon, i) => console.log(i, icon));
-			//Object.entries(iconMap).forEach(([key, value], index) => console.log(index, key, value));
-			break;
-		case 'e':
-			console.clear();
-			break;
-	}
-});
+progress.updateDom();
